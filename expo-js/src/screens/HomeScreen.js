c1c8ab1661ada33d2995fc01c78b637dc6a654af
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2023-02-11 21:06:54 +0100

    fix: prettier

--- expo-js/src/screens/HomeScreen.js
+++ expo-js/src/screens/HomeScreen.js
@@ -1,14 +1,18 @@
-import * as React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import * as React from "react";
+import { TouchableOpacity, View, Text } from "react-native";
 
-const HomeScreen = ({navigation}) => {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => { navigation.navigate('InputEmailScreen') } }>
-                <Text>Input Email Screen</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("InputEmailScreen");
+        }}
+      >
+        <Text>Input Email Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default HomeScreen;
